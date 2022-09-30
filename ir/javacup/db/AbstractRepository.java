d1de4654c75b5fa49eaef5a0fd177b80e2cda2e8
J-IOProgrammer <fg.bonny@outlook.com>
2022-09-30 13:11:32 +0330

    add implemets methods to AbstractRepository class and compelete the save method

--- ir/javacup/db/AbstractRepository.java
+++ ir/javacup/db/AbstractRepository.java
@@ -1,31 +1,40 @@
 package ir.javacup.db;
 
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
-public abstract class AbstractRepository implements IRepository {
+public abstract class AbstractRepository<U, T extends IEntity<U>> implements IRepository<U, T> {
+
+    private Map<U, T> map = new HashMap<>();
+    private IdGenerator<U> idGenerator;
 
     @Override
-    public void save(IEntity entity) {
+    public void save(T entity) {
+        if (entity == null)
+            throw new IllegalArgumentException("Object is null");
+        U id = idGenerator.generate();
+        entity.setId(id);
+        map.put(id, entity);
+    }
+
+    @Override
+    public void update(T entity) {
 
     }
 
     @Override
-    public void update(IEntity entity) {
-
-    }
-
-    @Override
-    public IEntity load(Object id) {
+    public T load(U id) {
         return null;
     }
 
     @Override
-    public List loadAll() {
+    public List<T> loadAll() {
         return null;
     }
 
     @Override
-    public void delete(Object id) {
+    public void delete(U id) {
 
     }
 
